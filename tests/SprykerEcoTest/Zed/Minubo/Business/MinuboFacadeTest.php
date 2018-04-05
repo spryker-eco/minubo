e76e4191f33772d6d26db9b9747e28097c1e7683
Valerii Pravoslavnyi <valerio8787@gmail.com>
2018-04-05 12:29:20 +0300

    feature/eco-1259-minubo-integration * Updated tests configuration

---
+++ tests/SprykerEcoTest/Zed/Minubo/Business/MinuboFacadeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEcoTest\Zed\Minubo\Business;
+
+use Codeception\TestCase\Test;
+
+class MinuboFacadeTest extends Test
+{
+    /**
+     * @return void
+     */
+    public function testMinuboFacade(): void
+    {
+
+    }
+}
